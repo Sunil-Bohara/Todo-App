7dfe7898b8acb19ebcbb694207320d867a1ba301
Sunil Bohara <sunilbohara2142060@gmail.com>
2023-03-30 18:16:02 +0530

    Added javascript and some styling improvements

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+const todoForm = document.querySelector(".todo-section-form");
+const todoInput = document.querySelector(".todo-section-form input[type='text']");
+const todoList = document.querySelector(".todo-list");
+// console.log(todoInput);
+todoForm.addEventListener("submit", (e) => {
+    // console.log(todoInput.value);
+    e.preventDefault();
+    const newTodoText = todoInput.value;
+    const newTodoList = document.createElement("li");
+    const newTodoListInnerHTML = `<span class="span-text">${newTodoText}</span>
+                                  <div class="btn">
+                                        <button type="submit">Done</button>
+                                        <button type="submit">Remove</button>
+                                  </div>`
+    newTodoList.innerHTML = newTodoListInnerHTML;
+    todoList.append(newTodoList);                              
+    todoInput.value = "";
+})
